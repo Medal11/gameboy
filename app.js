c20ecb97680fc13d708d714ca7a6232b98d26370
Medal11 <daniel.p.rabiner@gmail.com>
2025-02-15 15:08:21 -0500

    Create app.js

---
+++ app.js
@@ -0,0 +1,13 @@
+// JavaScript to handle game selection
+function startGame(game) {
+    if (game === 'snake') {
+        alert("Snake game will start soon!");
+        // Here, you can redirect to the Snake game page or load the game logic.
+    } else if (game === 'frogger') {
+        alert("Frogger game will start soon!");
+        // Redirect to Frogger or load game logic.
+    } else if (game === 'pacman') {
+        alert("Pacman game will start soon!");
+        // Add Pacman logic or redirection.
+    }
+}
